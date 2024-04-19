11301186f0ca0edb1835f3a0bd744099a281a8bf
vesnastokic <vesnastokic019@gmail.com>
2024-04-19 12:37:45 -0500

    New styling changes of articles

--- src/components/ArticlesPage.js
+++ src/components/ArticlesPage.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React from "react";
+import "./ArticlesPage.css";
 
 const articles = [
   {
-    title: '9 WAYS TO BOOST YOUR SOCIAL LIFE AS A BUSY MUM',
-    description: 'Studies show, those with social support live two and a half years longer that those who are isolated!Read how.',
-    url: 'http://www.multiculturalmotherhood.com/2019/02/9-ways-to-boost-your-social-life-as-busy-mum.html'
+    title: "9 Ways to Boost Your Social Life As A Busy Mum",
+    description:
+      "Studies show, those with social support live two and a half years longer that those who are isolated!Read how.",
+    url: "http://www.multiculturalmotherhood.com/2019/02/9-ways-to-boost-your-social-life-as-busy-mum.html",
   },
   {
-    title: 'HEALTHY EATING HABITS FOR KIDS',
-    description: 'Discover the importance of instilling healthy eating habits in your children and how to do it effectively.',
-    url: 'https://www.myplate.gov/tip-sheet/healthy-eating-kids'
+    title: "Healthy Eating Habits For Kids",
+    description:
+      "Discover the importance of instilling healthy eating habits in your children and how to do it effectively.",
+    url: "https://www.myplate.gov/tip-sheet/healthy-eating-kids",
+  },
+  {
+    title: "No, I am Not Just a Stay-at-Home Mom",
+    description:
+      "The mental load of motherhood is real, but our obsessive need to do something “more” can make us feel unbearable.",
+    url: "https://www.nytimes.com/2020/04/17/parenting/stay-at-home-mom.html?auth=login-google1tap&login=google1tap",
+  },
+  {
+    title: "What I wish I knew about being a stay-at-home mom",
+    description:
+      "There are a few things I wish I could tell my new-stay-at-home-mom-self. But of course, I can not. So instead I will say them to you.",
+    url: "https://www.mother.ly/life/stay-at-home-mom-advice/",
   },
   // Add more articles later
 ];
 
 const ArticlesPage = () => {
   return (
-    <div>
+    <div className="articles-container">
       <h1>Family Articles</h1>
-      <ul>
+      <ul className="article-list"> {/* Changed to match the CSS class */}
         {articles.map((article, index) => (
-          <li key={index}>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
+          <li key={index} className="article-item"> {/* Optional: Added article-item class */}
+            <a href={article.url} target="_blank" rel="noopener noreferrer" className="article-link">
               <h3>{article.title}</h3>
               <p>{article.description}</p>
             </a>
